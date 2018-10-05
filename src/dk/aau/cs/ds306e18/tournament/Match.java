989c1b0dc46a309083a677c808be572fbde0d808
unknown <nicoesterby@gmail.com>
2018-10-05 20:02:15 +0200

    Added match results.

--- src/dk/aau/cs/ds306e18/tournament/Match.java
+++ src/dk/aau/cs/ds306e18/tournament/Match.java
@@ -4,10 +4,40 @@ import dk.aau.cs.ds306e18.tournament.participants.Participant;
 
 public class Match {
 
+    private MatchResult result;
     private Participant blueTeam;
     private Participant orangeTeam;
     private Match bluePriorMatch;
     private Match orangePriorMatch;
+
+    public boolean isReadyToPlay() {
+        return (bluePriorMatch != null && bluePriorMatch.hasBeenPlayed()
+                && orangePriorMatch != null && orangePriorMatch.hasBeenPlayed());
+    }
+
+    public boolean hasBeenPlayed() {
+        return result != null;
+    }
+
+    public void setResult(MatchResult result) {
+        this.result = result;
+    }
+
+    public Participant getWinner() {
+        switch (result.getConclusion()) {
+            case BLUE_WINS: return blueTeam;
+            case ORANGE_WINS: return orangeTeam;
+            default: return blueTeam; // TODO Add TieBreaker
+        }
+    }
+
+    public Participant getLoser() {
+        switch (result.getConclusion()) {
+            case BLUE_WINS: return orangeTeam;
+            case ORANGE_WINS: return blueTeam;
+            default: return orangeTeam; // TODO Add TieBreaker
+        }
+    }
 
     public Participant getBlueTeam() {
         return blueTeam;
