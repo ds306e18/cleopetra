57468a5837c1274e1e2ab371ddbd052b890582fb
unknown <nicoesterby@gmail.com>
2018-10-13 11:35:16 +0200

    Fixed tests to use new match linking.

--- test/dk/aau/cs/ds306e18/tournament/model/MatchTest.java
+++ test/dk/aau/cs/ds306e18/tournament/model/MatchTest.java
@@ -8,14 +8,15 @@ public class MatchTest {
 
     @Test
     public void isReadyToPlay01() {
-        Match match = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
+        Match match = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
         assertTrue(match.isReadyToPlay());
     }
 
     @Test
     public void isReadyToPlay02() {
-        Match firstMatch = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
-        Match secondMatch = new Match(new StarterSlot(new Team("C", null, 0, "c")), new WinnerOf(firstMatch));
+        Match firstMatch = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
+        Match secondMatch = new Match(new Team("C", null, 0, "c"), null);
+        secondMatch.useWinnerFrom(firstMatch, false);
         assertFalse(secondMatch.isReadyToPlay());
 
         firstMatch.setScores(4, 2, true);
@@ -27,7 +28,7 @@ public class MatchTest {
         // team blue wins
         Team expectedWinner = new Team("A", null, 0, "a");
         Team expectedLoser = new Team("B", null, 0, "b");
-        Match match = new Match(new StarterSlot(expectedWinner), new StarterSlot(expectedLoser));
+        Match match = new Match(expectedWinner, expectedLoser);
         match.setScores(3, 2, true);
         assertSame(match.getWinner(), expectedWinner);
         assertSame(match.getLoser(), expectedLoser);
@@ -38,7 +39,7 @@ public class MatchTest {
         // team orange wins
         Team expectedWinner = new Team("A", null, 0, "a");
         Team expectedLoser = new Team("B", null, 0, "b");
-        Match match = new Match(new StarterSlot(expectedLoser), new StarterSlot(expectedWinner));
+        Match match = new Match(expectedLoser, expectedWinner);
         match.setScores(3, 5, true);
         assertSame(match.getWinner(), expectedWinner);
         assertSame(match.getLoser(), expectedLoser);
@@ -46,50 +47,52 @@ public class MatchTest {
 
     @Test(expected = IllegalStateException.class)
     public void getWinnerAndLoser03() {
-        Match match = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
+        Match match = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
         match.setScores(3, 5); // note: match is not finished
         match.getWinner();
     }
 
     @Test(expected = IllegalStateException.class)
     public void getWinnerAndLoser04() {
-        Match match = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
+        Match match = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
         match.setScores(3, 5); // note: match is not finished
         match.getLoser();
     }
 
     @Test
     public void getStatus01() {
-        Match firstMatch = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
-        Match secondMatch = new Match(new StarterSlot(new Team("C", null, 0, "c")), new WinnerOf(firstMatch));
+        Match firstMatch = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
+        Match secondMatch = new Match(new Team("C", null, 0, "c"), null);
+        secondMatch.useWinnerFrom(firstMatch, false);
         assertSame(secondMatch.getStatus(), MatchStatus.NOT_PLAYABLE);
     }
 
     @Test
     public void getStatus02() {
-        Match firstMatch = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
-        Match secondMatch = new Match(new StarterSlot(new Team("C", null, 0, "c")), new WinnerOf(firstMatch));
+        Match firstMatch = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
+        Match secondMatch = new Match(new Team("C", null, 0, "c"), null);
+        secondMatch.useWinnerFrom(firstMatch, false);
         firstMatch.setScores(0, 2, true);
         assertSame(secondMatch.getStatus(), MatchStatus.READY_TO_BE_PLAYED);
     }
 
     @Test
     public void getStatus03() {
-        Match match = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
+        Match match = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
         match.setScores(0, 0, true);
         assertSame(match.getStatus(), MatchStatus.DRAW);
     }
 
     @Test
     public void getStatus04() {
-        Match match = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
+        Match match = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
         match.setScores(2, 0, true);
         assertSame(match.getStatus(), MatchStatus.BLUE_WINS);
     }
 
     @Test
     public void getStatus05() {
-        Match match = new Match(new StarterSlot(new Team("A", null, 0, "a")), new StarterSlot(new Team("B", null, 0, "b")));
+        Match match = new Match(new Team("A", null, 0, "a"), new Team("B", null, 0, "b"));
         match.setScores(0, 2, true);
         assertSame(match.getStatus(), MatchStatus.ORANGE_WINS);
     }
