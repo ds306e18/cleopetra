230c7f3c021d977bd4657ee65e26df217db5b633
Christopher Buchardt <christopher.buchardt@gmail.com>
2018-10-24 11:08:44 +0200

    Added Launcher class with non-functioning buttons

--- src/dk/aau/cs/ds306e18/tournament/Main.java
+++ src/dk/aau/cs/ds306e18/tournament/Main.java
@@ -1,7 +1,19 @@
 package dk.aau.cs.ds306e18.tournament;
 
-public class Main {
+import dk.aau.cs.ds306e18.tournament.UI.Launcher;
+
+
+
+public class Main  {
     public static void main(String[]args){
         System.out.println("hola mundo");
+        startLauncher(args);
+
+
     }
+
+    private static void startLauncher(String[] args) {
+        Launcher launcher = new Launcher().startLauncher(args);
+    }
+
 }
