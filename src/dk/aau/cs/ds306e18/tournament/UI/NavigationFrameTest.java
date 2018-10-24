994e5cb6d2976cb3833159d7324672e8ea74d0f8
Mikkel K <32062449+judo347@users.noreply.github.com>
2018-10-24 11:31:36 +0200

    added comments to NavigationFrame + NavigationFrameTest.

--- src/dk/aau/cs/ds306e18/tournament/UI/NavigationFrameTest.java
+++ src/dk/aau/cs/ds306e18/tournament/UI/NavigationFrameTest.java
@@ -1,5 +1,6 @@
 package dk.aau.cs.ds306e18.tournament.UI;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import java.util.ArrayList;
 
@@ -9,12 +10,16 @@ public class NavigationFrameTest extends NavigationFrame {
     @Override
     ArrayList<Tab> addContent() {
 
+        //Create the tabs
         Tab tab1 = new Tab();
         Tab tab2 = new Tab();
 
+        //Set content and attributes of tabs
         tab1.setText("Test 1");
         tab2.setText("Test 2");
+        tab1.setContent(new Label("Hello"));
 
+        //Add tabs to arrayList and return it
         ArrayList<Tab> tabs = new ArrayList<>();
         tabs.add(tab1);
         tabs.add(tab2);
