678ff7d651a671939f23de49abb6e395e2534677
Mikkel K <32062449+judo347@users.noreply.github.com>
2018-10-26 10:02:08 +0200

    created the SwissNode that can represent a swiss stage visually.

---
+++ src/dk/aau/cs/ds306e18/tournament/UI/bracketObjects/SwissNode.java
@@ -0,0 +1,40 @@
+package dk.aau.cs.ds306e18.tournament.UI.bracketObjects;
+
+import dk.aau.cs.ds306e18.tournament.model.Match;
+import dk.aau.cs.ds306e18.tournament.model.SwissStage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+/** Used to display the a swiss stage. */
+public class SwissNode extends VBox {
+
+    /** Used to display the a swiss stage. */
+    public SwissNode(SwissStage swissStage){
+        refreshMatches(swissStage);
+    }
+    
+    /** Refreshes this vbox to represent the given swiss stage.
+     * @param swissStage the swiss stage to represent. */ //TODO Should be rename and reworked.
+    private void refreshMatches(SwissStage swissStage){
+
+        ArrayList<ArrayList<Match>> rounds = swissStage.getRounds();
+
+        //Go through all rounds.
+        for(int i = 0; i < rounds.size(); i++){
+
+            //Add label and matches for each round.
+            Label label = new Label("Round " + i);
+            ArrayList<VisualMatch> visualMatches = new ArrayList<>();
+
+            for(Match match : rounds.get(i)){
+                visualMatches.add(new VisualMatch(match));
+            }
+
+            //Add label and matches for round.
+            getChildren().add(label);
+            getChildren().addAll(visualMatches);
+        }
+    }
+}
