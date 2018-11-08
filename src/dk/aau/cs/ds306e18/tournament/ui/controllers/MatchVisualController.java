5c818ad10900f39d707ad93aebf213b44daad5c0
Mikkel K <32062449+judo347@users.noreply.github.com>
2018-11-08 10:51:25 +0100

    matchVisual can now show a match by setting the propper tags and id based on the given match.

--- src/dk/aau/cs/ds306e18/tournament/ui/controllers/MatchVisualController.java
+++ src/dk/aau/cs/ds306e18/tournament/ui/controllers/MatchVisualController.java
@@ -1,8 +1,10 @@
 package dk.aau.cs.ds306e18.tournament.ui.controllers;
 
 import dk.aau.cs.ds306e18.tournament.model.Match;
-import javafx.event.ActionEvent;
+import dk.aau.cs.ds306e18.tournament.model.Team;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 public class MatchVisualController {
@@ -11,8 +13,11 @@ public class MatchVisualController {
     @FXML private Text teamOrangeScore;
     @FXML private Text textBlueName;
     @FXML private Text teamBlueScore;
+    @FXML private HBox hboxOrangeTeam;
+    @FXML private HBox hboxBlueTeam;
 
     private BracketOverviewTabController boc;
+    private Node myNodeObj;
     private Match lastSetMatch;
 
     @FXML
@@ -31,12 +36,98 @@ public class MatchVisualController {
         this.boc = boc;
     }
 
-    public void setMatch(Match match){
+    public void setMyNodeObj(Node node){
+        this.myNodeObj = node;
+    }
+
+    public void updateMatch(Match match){
+        this.myNodeObj.setId("TBD"); //Get status of match and set visual representation to match. //TODO TeMP
+        this.myNodeObj.setId("matchplayed");
         this.lastSetMatch = match;
         this.textBlueName.setText(match.getBlueTeam().getTeamName());
         this.textOrangeName.setText(match.getOrangeTeam().getTeamName());
         this.teamBlueScore.setText(String.valueOf(match.getBlueScore()));
         this.teamOrangeScore.setText(String.valueOf(match.getOrangeScore()));
+    }
+
+    /** Clears the visuals match for both text and css. */
+    private void clearFields(){
+        myNodeObj.setId("");
+        hboxBlueTeam.getStyleClass().clear();
+        hboxOrangeTeam.getStyleClass().clear();
+        textBlueName.setText("TBD");
+        textOrangeName.setText("TBD");
+        teamOrangeScore.setText("");
+        teamBlueScore.setText("");
+    }
+
+    public void updateMatch2(Match match){
+
+        clearFields();
+        this.lastSetMatch = match;
+
+        Team blueTeam = null;
+        Team orangeTeam = null;
+        try {
+            blueTeam = match.getBlueTeam();
+            orangeTeam = match.getOrangeTeam();
+        }catch (IllegalStateException e){
+
+        }
+
+        switch (match.getStatus()) {
+            case NOT_PLAYABLE:          //CSS
+                                        myNodeObj.setId("matchTBD");
+
+                                        //DATA
+                                        if(blueTeam != null)
+                                            textBlueName.setText(match.getBlueTeam().getTeamName());
+                                        if(orangeTeam != null)
+                                            textOrangeName.setText(match.getOrangeTeam().getTeamName());
+
+                break;
+            case READY_TO_BE_PLAYED:    //CSS
+                                        myNodeObj.setId("matchUpcoming");
+                                        hboxBlueTeam.getStyleClass().add("blue");
+                                        hboxBlueTeam.getStyleClass().add("team1");
+                                        hboxOrangeTeam.getStyleClass().add("orange");
+                                        hboxOrangeTeam.getStyleClass().add("team2");
+
+                                        //Data
+                                        textBlueName.setText(match.getBlueTeam().getTeamName());
+                                        textOrangeName.setText(match.getOrangeTeam().getTeamName());
+                break;
+            case BLUE_WINS:             //CSS
+                                        myNodeObj.setId("matchPlayed");
+                                        hboxBlueTeam.getStyleClass().add("blue");
+                                        hboxBlueTeam.getStyleClass().add("team1");
+                                        hboxBlueTeam.getStyleClass().add("winner");
+                                        hboxOrangeTeam.getStyleClass().add("orange");
+                                        hboxOrangeTeam.getStyleClass().add("team2");
+
+                                        //Data
+                                        textBlueName.setText(match.getBlueTeam().getTeamName());
+                                        textOrangeName.setText(match.getOrangeTeam().getTeamName());
+                                        teamBlueScore.setText(String.valueOf(match.getBlueScore()));
+                                        teamOrangeScore.setText(String.valueOf(match.getOrangeScore()));
+                break;
+            case ORANGE_WINS:           //CSS
+                                        myNodeObj.setId("matchPlayed");
+                                        hboxBlueTeam.getStyleClass().add("blue");
+                                        hboxBlueTeam.getStyleClass().add("team1");
+                                        hboxOrangeTeam.getStyleClass().add("orange");
+                                        hboxOrangeTeam.getStyleClass().add("team2");
+                                        hboxOrangeTeam.getStyleClass().add("winner");
+
+                                        //Data
+                                        textBlueName.setText(match.getBlueTeam().getTeamName());
+                                        textOrangeName.setText(match.getOrangeTeam().getTeamName());
+                                        teamBlueScore.setText(String.valueOf(match.getBlueScore()));
+                                        teamOrangeScore.setText(String.valueOf(match.getOrangeScore()));
+                break;
+            case DRAW:
+            default: throw new IllegalStateException();
+        }
     }
 
     public Match getLastSetMatch() {
