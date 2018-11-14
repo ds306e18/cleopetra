e36f26e174105da1803feee1abed2c7535bcdb28
NicEastvillage <nicoesterby@gmail.com>
2018-11-14 08:32:23 +0100

    Increased font size of groups and renamed in code.

--- src/dk/aau/cs/ds306e18/tournament/ui/bracketObjects/RoundRobinSettingsNode.java
+++ src/dk/aau/cs/ds306e18/tournament/ui/bracketObjects/RoundRobinSettingsNode.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 
 public class RoundRobinSettingsNode extends BorderPane {
 
@@ -13,17 +14,18 @@ public class RoundRobinSettingsNode extends BorderPane {
     public RoundRobinSettingsNode(RoundRobinFormat roundRobin) {
         this.roundRobin = roundRobin;
 
-        // Rounds label
-        Label roundsLabel = new Label("Groups:");
-        setLeft(roundsLabel);
+        // Groups label
+        Label groupsLabel = new Label("Groups:");
+        groupsLabel.setFont(new Font("System", 16));
+        setLeft(groupsLabel);
 
-        // Rounds spinner
-        Spinner<Integer> roundsSpinner = new Spinner<>(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, Integer.MAX_VALUE));
-        roundsSpinner.setEditable(true);
-        roundsSpinner.getValueFactory().setValue(roundRobin.getNumberOfGroups());
-        roundsSpinner.getValueFactory().valueProperty().addListener((observable, oldValue, newValue) -> {
+        // Groups spinner
+        Spinner<Integer> GroupsSpinner = new Spinner<>(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, Integer.MAX_VALUE));
+        GroupsSpinner.setEditable(true);
+        GroupsSpinner.getValueFactory().setValue(roundRobin.getNumberOfGroups());
+        GroupsSpinner.getValueFactory().valueProperty().addListener((observable, oldValue, newValue) -> {
             roundRobin.setNumberOfGroups(newValue);
         });
-        setRight(roundsSpinner);
+        setRight(GroupsSpinner);
     }
 }
