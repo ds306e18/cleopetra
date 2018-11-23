94ab1852cc3d5e6612e2c6b472e5e0542a9262f0
NicEastvillage <nicoesterby@gmail.com>
2018-11-23 11:16:08 +0100

    Added fxml for RLBot settings.

---
+++ src/dk/aau/cs/ds306e18/tournament/ui/RLBotSettingsTabController.java
@@ -0,0 +1,24 @@
+package dk.aau.cs.ds306e18.tournament.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class RLBotSettingsTabController {
+
+    @FXML private Button configPathBtn;
+    @FXML private TextField configPathTextField;
+    @FXML private CheckBox autoCloseRLBotCheckBox;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void configPathBtnOnAction(ActionEvent actionEvent) {
+
+    }
+}
