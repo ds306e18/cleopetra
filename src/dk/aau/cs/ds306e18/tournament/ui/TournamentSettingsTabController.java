3585236d14dd1a896f4744ebd80717305a3899c1
Mikkel K <32062449+judo347@users.noreply.github.com>
2018-12-07 09:50:44 +0100

    tournamentSettingsTab: stage listview now displays a number + name of stage.

--- src/dk/aau/cs/ds306e18/tournament/ui/TournamentSettingsTabController.java
+++ src/dk/aau/cs/ds306e18/tournament/ui/TournamentSettingsTabController.java
@@ -43,8 +43,8 @@ public class TournamentSettingsTabController {
 
     @FXML
     private void initialize() {
-        /* Assign items to the list in case of a tournament being loaded */
-        stagesListView.setItems(FXCollections.observableArrayList(Tournament.get().getStages()));
+
+        setUpStageListView();
 
         /* Retrieve and set tournament name into textfield. */
         nameTextField.setText(Tournament.get().getName());
@@ -76,29 +76,6 @@ public class TournamentSettingsTabController {
             }
         }
 
-        /* By default the stage settings are hidden.
-         * This listener is used to show the stage settings when there is at least one Stage added.
-         * Also handles disabling and enabling of buttons for stages. */
-        stagesListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            stageSettingsVBox.setVisible(stagesListView.getItems().size() != 0);
-
-            /* Handle stage order button disabling / enabling */
-            int selectedIndex = getSelectedIndex();
-            swapUp.setDisable(selectedIndex == 0);
-            swapDown.setDisable(selectedIndex == stagesListView.getItems().size() - 1 && selectedIndex != -1);
-
-            /* Set content inside stage settings to show chosen stage */
-            showStageValues();
-
-            /* If the stageListView has no items. Then the remove, up and down buttons is disabled. */
-            if(stagesListView.getItems().size() == 0) {
-                removeStageBtn.setDisable(true);
-                swapUp.setDisable(true);
-                swapDown.setDisable(true);
-            } else
-                removeStageBtn.setDisable(false);
-        });
-
         /* Setup teams wanted in stage spinner */
         teamsInStageSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(2, Integer.MAX_VALUE));
         teamsInStageSpinner.getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
@@ -121,6 +98,52 @@ public class TournamentSettingsTabController {
             }
 
             updateFormatUniqueSettings();
+        });
+    }
+
+    /** Sets up the listview for stages. Settings items,
+     * adding listener and changing what is displayed. */
+    private void setUpStageListView(){
+
+        /* Assign items to the list in case of a tournament being loaded */
+        stagesListView.setItems(FXCollections.observableArrayList(Tournament.get().getStages()));
+
+        /* Formatting what is displayed in the listView: id + name. */
+        stagesListView.setCellFactory(lv -> new ListCell<Stage>(){
+            public void updateItem(Stage stage, boolean empty) {
+                super.updateItem(stage, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    int index = stagesListView.getItems().indexOf(stage) + 1;
+                    String text = "#" + index + " " + stage.getName();
+                    setText(text);
+                    stagesListView.refresh();
+                }
+            }
+        });
+
+        /* By default the stage settings are hidden.
+         * This listener is used to show the stage settings when there is at least one Stage added.
+         * Also handles disabling and enabling of buttons for stages. */
+        stagesListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            stageSettingsVBox.setVisible(stagesListView.getItems().size() != 0);
+
+            /* Handle stage order button disabling / enabling */
+            int selectedIndex = getSelectedIndex();
+            swapUp.setDisable(selectedIndex == 0);
+            swapDown.setDisable(selectedIndex == stagesListView.getItems().size() - 1 && selectedIndex != -1);
+
+            /* Set content inside stage settings to show chosen stage */
+            showStageValues();
+
+            /* If the stageListView has no items. Then the remove, up and down buttons is disabled. */
+            if(stagesListView.getItems().size() == 0) {
+                removeStageBtn.setDisable(true);
+                swapUp.setDisable(true);
+                swapDown.setDisable(true);
+            } else
+                removeStageBtn.setDisable(false);
         });
     }
 
