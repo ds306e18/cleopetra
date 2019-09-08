9dbc3ab5599730f5c8a63e0fd1c60527048eac72
NicEastvillage <nicoesterby@gmail.com>
2019-09-08 10:58:14 +0200

    Improved Table size and cleaned TableStats.

--- src/dk/aau/cs/ds306e18/tournament/ui/StatsTable.java
+++ src/dk/aau/cs/ds306e18/tournament/ui/StatsTable.java
@@ -5,13 +5,10 @@ import dk.aau.cs.ds306e18.tournament.model.format.Format;
 import dk.aau.cs.ds306e18.tournament.model.stats.Stats;
 import dk.aau.cs.ds306e18.tournament.model.stats.StatsChangeListener;
 import dk.aau.cs.ds306e18.tournament.ui.bracketObjects.ModelCoupledUI;
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,73 +19,103 @@ public class StatsTable extends TableView<Stats> implements StatsChangeListener,
     private List<Team> teams;
     private Format format;
 
+    /**
+     * Create a StatsTable to show stats for the given teams in the given stage/format. If format is null global stats
+     * will be shown instead.
+     */
     public StatsTable(List<Team> teams, Format format) {
         super();
         this.teams = new ArrayList<>(teams);
         this.format = format;
         setFocusTraversable(false);
         setMouseTransparent(true);
-        setPrefHeight(0); // Table heights are a nightmare. This creates an okay result.
-        setPrefWidth(350);
+        calculateSize();
         update();
         registerAsListener();
     }
 
-    public void update() {
-        // Clear everything inside the tableView
+    protected void calculateSize() {
+        int items = teams.size();
+        setPrefHeight(23.5 + 24.5 * items); // Table heights are a nightmare. This creates an okay result.
+        setPrefWidth(345);
+        setMinHeight(USE_PREF_SIZE);
+        setMaxHeight(USE_PREF_SIZE);
+    }
+
+    /**
+     * Clears the table for columns and cells.
+     */
+    public void clear() {
         getItems().clear();
         getColumns().clear();
         getSortOrder().clear();
+    }
 
-        // Find stats objects to display
+    /**
+     * Adds the relevant Stats items to the table. If format is null global stats will be used.
+     */
+    protected void prepareItems() {
         List<Stats> stats = teams.stream()
                 .map(t -> format == null ? t.getStatsManager().getGlobalStats() : t.getStatsManager().getStats(format))
                 .collect(Collectors.toList());
         getItems().addAll(stats);
+    }
 
-        // Team name column
+    protected void addTeamNameColumn() {
         TableColumn<Stats, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getTeam().getTeamName()));
-        nameColumn.setMaxWidth(140);
+        nameColumn.setMinWidth(150);
+        nameColumn.setMaxWidth(150);
+        getColumns().add(nameColumn);
+    }
 
-        // Wins column
+    protected void addWinsColumn() {
         TableColumn<Stats, Integer> winsColumn = new TableColumn<>("Wins");
         winsColumn.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getWins()).asObject());
+        winsColumn.setSortType(TableColumn.SortType.DESCENDING);
+        winsColumn.setStyle("-fx-alignment: CENTER;");
+        getColumns().add(winsColumn);
+        getSortOrder().add(winsColumn);
+    }
 
-        // Wins column
+    protected void addLosesColumn() {
         TableColumn<Stats, Integer> losesColumn = new TableColumn<>("Loses");
         losesColumn.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getLoses()).asObject());
+        losesColumn.setSortType(TableColumn.SortType.ASCENDING);
+        losesColumn.setStyle("-fx-alignment: CENTER;");
+        getColumns().add(losesColumn);
+        getSortOrder().add(losesColumn);
+    }
 
-        // Goal difference column
+    protected void addGoalDiffColumn() {
         TableColumn<Stats, Integer> goalDifferenceColumn = new TableColumn<>("GDiff");
         goalDifferenceColumn.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getGoalDifference()).asObject());
+        goalDifferenceColumn.setSortType(TableColumn.SortType.DESCENDING);
+        goalDifferenceColumn.setStyle("-fx-alignment: CENTER;");
+        getColumns().add(goalDifferenceColumn);
+        getSortOrder().add(goalDifferenceColumn);
+    }
 
-        // Goals column
+    protected void addGoalsColumn() {
         TableColumn<Stats, Integer> goalsColumn = new TableColumn<>("Goals");
         goalsColumn.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getGoals()).asObject());
-
-        // Column sorting
-        winsColumn.setSortType(TableColumn.SortType.DESCENDING);
-        losesColumn.setSortType(TableColumn.SortType.ASCENDING);
-        goalDifferenceColumn.setSortType(TableColumn.SortType.DESCENDING);
         goalsColumn.setSortType(TableColumn.SortType.DESCENDING);
-
-        // Add columns and sorting orders
-        getColumns().add(nameColumn);
-        getColumns().add(winsColumn);
-        getColumns().add(losesColumn);
-        getColumns().add(goalDifferenceColumn);
-        getColumns().add(goalsColumn);
-        getSortOrder().add(winsColumn);
-        getSortOrder().add(losesColumn);
-        getSortOrder().add(goalDifferenceColumn);
-        getSortOrder().add(goalsColumn);
-
-        // Additional styling
-        winsColumn.setStyle("-fx-alignment: CENTER;");
-        losesColumn.setStyle("-fx-alignment: CENTER;");
-        goalDifferenceColumn.setStyle("-fx-alignment: CENTER;");
         goalsColumn.setStyle("-fx-alignment: CENTER;");
+        getColumns().add(goalsColumn);
+        getSortOrder().add(goalsColumn);
+    }
+
+    /**
+     * Updates all values in the table.
+     */
+    public void update() {
+        clear();
+        prepareItems();
+        addTeamNameColumn();
+        addWinsColumn();
+        addLosesColumn();
+        addGoalDiffColumn();
+        addGoalsColumn();
     }
 
     private void registerAsListener() {
