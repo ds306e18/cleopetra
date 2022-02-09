fdbfeb7f3b37763b17b211f347decc47dd40b8fd
NicEastvillage <nicoesterby@gmail.com>
2022-02-09 20:25:56 +0100

    Fix save bug and update gradle stuff.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,14 @@ module cleopetra {
     requires org.controlsfx.controls;
 
     exports dk.aau.cs.ds306e18.tournament;
+    opens dk.aau.cs.ds306e18.tournament.model;
+    opens dk.aau.cs.ds306e18.tournament.model.format;
+    opens dk.aau.cs.ds306e18.tournament.model.match;
+    opens dk.aau.cs.ds306e18.tournament.model.stats;
+    opens dk.aau.cs.ds306e18.tournament.rlbot;
+    opens dk.aau.cs.ds306e18.tournament.rlbot.configuration;
+    opens dk.aau.cs.ds306e18.tournament.serialization;
+
     opens dk.aau.cs.ds306e18.tournament.ui;
     opens dk.aau.cs.ds306e18.tournament.utility;
 }
